f0477191ffd2ace44b798f0025ff75c7d157147b
Leonardo Reis <leonardoreis.ufsj@gmail.com>
2024-05-26 14:41:20 -0300

    [Hotfix] Router-dom

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import "../styles/globals.css";
 
-import { BrowserRouter as Router } from "react-router-dom";
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
