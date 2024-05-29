8aee28c4688732ae0ea449f4d1f110ae8e1c0fea
Leonardo Reis <leonardoreis.ufsj@gmail.com>
2024-05-29 19:18:04 -0300

    [Feature] Contact Form.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import { BsCardText } from "react-icons/bs";
+import { GiBrazilFlag } from "react-icons/gi";
+import { FaFlagUsa } from "react-icons/fa";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 import profile from "../public/profile.png";
@@ -33,13 +34,23 @@ export default function Home() {
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
                   className=" cursor-pointer text-2xl"
+                  title="Night Mode"
                 />
               </li>
               <li class="px-8">
-                <BsCardText
-                  onClick={() => setLanguage(!language)}
-                  className=" cursor-pointer text-2xl"
-                />
+                {language ? (
+                  <GiBrazilFlag
+                    onClick={() => setLanguage(!language)}
+                    className=" cursor-pointer text-2xl"
+                    title="PT-BR"
+                  />
+                ) : (
+                  <FaFlagUsa
+                    onClick={() => setLanguage(!language)}
+                    className=" cursor-pointer text-2xl"
+                    title="EN"
+                  />
+                )}
               </li>
               <li>
                 <a
@@ -212,9 +223,17 @@ export default function Home() {
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">
-              {language ? <div>Portfolio</div> : <div> Portfólio</div>}
-            </h3>
+            <span className="text-teal-500">
+              {language ? (
+                <h3 className="text-3xl py-1 dark:text-white ">
+                  <div>Portfolio</div>
+                </h3>
+              ) : (
+                <h3 className="text-3xl py-1 dark:text-white ">
+                  <div> Portfólio</div>
+                </h3>
+              )}
+            </span>
             {language ? (
               <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
                 Since the beginning of my career, I have worked with
@@ -306,6 +325,70 @@ export default function Home() {
                 }
               />
             </div>
+          </div>
+        </section>
+        <br />
+
+        <section class="bg-white dark:bg-gray-900">
+          <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 class="text-teal-500 mb-4 text-4xl tracking-tight font-extrabold text-center dark:text-white">
+              Contato
+            </h2>
+            <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+              Got a technical issue? Want to send feedback about a beta feature?
+              Need details about our Business plan? Let us know.
+            </p>
+            <form action="#" class="space-y-8">
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="subject"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  id="subject"
+                  class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="Let us know how we can help you"
+                  required
+                />
+              </div>
+              <div class="sm:col-span-2">
+                <label
+                  for="message"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                >
+                  Your message
+                </label>
+                <textarea
+                  id="message"
+                  rows="6"
+                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Leave a comment..."
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Send message
+              </button>
+            </form>
           </div>
         </section>
         <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
