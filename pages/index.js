e8e42ea6cea894937ef3a8b330b1ff5d01cfe5bf
Leonardo Reis <leonardoreis.ufsj@gmail.com>
2024-05-29 19:47:24 -0300

    [Hotfix] Contact form removido temporariamente.

--- pages/index.js
+++ pages/index.js
@@ -329,30 +329,20 @@ export default function Home() {
         </section>
         <br />
 
-        <section class="bg-white dark:bg-gray-900">
+        {/* <section class="bg-white dark:bg-gray-900">
           <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
             <h2 class="text-teal-500 mb-4 text-4xl tracking-tight font-extrabold text-center dark:text-white">
               Contato
             </h2>
             <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-              Got a technical issue? Want to send feedback about a beta feature?
-              Need details about our Business plan? Let us know.
+              Please contact me directly at
+              <a href="mailto:leonardoreis.ufsj@gmail.com" class="underline">
+                <p>leonardoreis.ufsj@gmail.com</p>
+              </a>
+              or through this form.
             </p>
+
             <form action="#" class="space-y-8">
-              <div>
-                <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  required
-                />
-              </div>
               <div>
                 <label
                   for="subject"
@@ -382,15 +372,18 @@ export default function Home() {
                   placeholder="Leave a comment..."
                 ></textarea>
               </div>
+
               <button
                 type="submit"
-                class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
               >
-                Send message
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Submit
+                </span>
               </button>
             </form>
           </div>
-        </section>
+        </section> */}
         <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
           <a
             href="https://www.linkedin.com/in/leoreiis/"
